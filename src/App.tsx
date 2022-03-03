4d3fa055691589b602263bf72973f07b70eda2fa
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-03 18:53:00 +0100

    added package react-router-dom and usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      OK
-    </div>
+    <Switch>
+      <Route path="/">
+        <div className="App">
+          OK
+        </div>
+      </Route>
+    </Switch>
   );
 }
 

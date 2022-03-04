dc1e7127f55cb5c01f87adf98dee7f81781caadb
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-04 09:56:11 +0100

    added global.d.ts file

---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+// Image assets
+declare module '*.jpg' {
+  export default '' as string;
+}
+declare module '*.png' {
+  export default '' as string;
+}
+declare module '*.svg' {
+  export default '' as string;
+}
+
+// Video assets
+declare module '*.mp4' {
+  export default '' as string;
+}

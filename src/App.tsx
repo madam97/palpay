070432be4c5b03ffa374d21f1bc4efe304466c46
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-15 21:54:22 +0100

    added Header component and its usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <main>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </main>
+    <>
+      <Header />
+
+      <main>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose, AiFillBank } from 'react-icons/ai';
+import { MdAttachMoney } from 'react-icons/md';
+import { FaRegAddressCard } from 'react-icons/fa';
+import { BsArrowLeftRight } from 'react-icons/bs';
+import {ReactComponent as Logo} from '../assets/logo.svg';
+import '../scss/components/header.scss';
+
+export default function Header() {
+
+  const [opened, setOpened] = useState<boolean>(false);
+
+  const toggleNav = (event: React.FormEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    setOpened(!opened);
+  }
+
+  // ---------------------------------------
+
+  return (
+    <header className="navbar">
+      <nav>
+        <Link to="/">
+          <Logo width="150" />
+        </Link>
+
+        <button className="navbar-toggler ml-auto" type="button" onClick={toggleNav}>
+          {!opened && <AiOutlineMenu className="icon icon-md" />}
+          {opened && <AiOutlineClose className="icon icon-md" />}
+        </button>
+
+        <ul className={`nav ${opened ? 'opened' : 'closed'}`}>
+
+          <li className="nav-divider">Transactions</li>
+          <li>
+            <Link to="/transaction/history">
+              <BsArrowLeftRight className="icon icon-mr" /> History
+            </Link>
+          </li>
+          <li>
+            <Link to="/transaction/new">
+              <MdAttachMoney className="icon icon-mr" /> New transaction
+            </Link>
+          </li>
+
+          <li className="nav-divider">Profile</li>
+          <li>
+            <Link to="/profile/bank-account">
+              <AiFillBank className="icon icon-mr" /> Bank account
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile/contacts">
+              <FaRegAddressCard className="icon icon-mr" /> Contacts
+            </Link>
+          </li>
+
+        </ul>
+      </nav>
+    </header>
+  )
+}
